e3d7320f00a0ac2e5228a15e0f48e4d231452ef1
Christian Maticorena <hola@eliottack.com>
2020-07-15 17:13:55 -0500

    Se añadio id en el form del login

--- users/vista/index.php
+++ users/vista/index.php
@@ -4,9 +4,13 @@
 		<title></title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+		<meta name="description" content="Lambda cursos online">
 		<link rel="stylesheet" type="text/css" href="../../css/app.css">
 		<script src="js/vendor/jquery-1.10.2.min.js"></script>
 		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="assets/css/overhang.min.css" />
 	</head>
 	<body>
 		<div class="wrap">
@@ -16,7 +20,7 @@
 					<div class="lateral__wellcome">Bienvenido</div>
 				</header>
 				<div class="lateral__form">
-					<form class="login__form" action="validarCode.php" method="POST" role="form">
+					<form class="login__form" id="loginForm" action="validarCode.php" method="POST" role="form">
 					<div class="form-group">
 						<input type="text" name="txtUsuario" class="form-control" id="usuario" autofocus required placeholder="Email">
 					</div>
@@ -38,6 +42,11 @@
 			</div>
 		</div>
 	<script src="js/main.js"></script>
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/overhang.min.js"></script>
+    <script src="assets/js/app.js"></script>
 	</body>
 </html>
 
