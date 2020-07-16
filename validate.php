fa956105fd933f50531814800c8335dac8a165a6
Christian Maticorena <hola@eliottack.com>
2020-07-16 11:38:33 -0500

    Se agrego envio de email y validacion

---
+++ validate.php
@@ -0,0 +1,15 @@
+<?php
+	require_once './users/vista/config.php';
+    $idmail = $_GET['id'];
+    
+    echo "validado";
+    
+	// $sql = "SELECT * FROM sorteos WHERE sor_id=:sorteo";
+	// $query = $pdo->prepare($sql);
+	// $query->execute([
+	// 	'sorteo' => $sorteo
+	// ]);
+
+	// $row = $query->fetch(PDO::FETCH_ASSOC);
+	// $idescortsValor = $row['sor_idescorts'];
+?>
